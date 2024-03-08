61cb4eec02e593ba83441b13d770db9f51dc3acc
gracenho829 <gracenho829@gmail.com>
2024-03-08 15:01:59 -0500

    first initialization of iOS app project

---
+++ ClapEducationiOS/ClapEducationiOS/ClapEducationiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClapEducationiOSApp.swift
+//  ClapEducationiOS
+//
+//  Created by Grace Nho on 2024/03/08.
+//
+
+import SwiftUI
+
+@main
+struct ClapEducationiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClapEducationiOS/ClapEducationiOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClapEducationiOS
+//
+//  Created by Grace Nho on 2024/03/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
