98ae103e707fba49859bcbe068afc3a9a4ee0983
gracenho829 <gracenho829@gmail.com>
2024-03-21 17:49:14 -0400

    created User data and login view

---
+++ ClapEducationiOS/ClapEducationiOS/Models/User.swift
@@ -0,0 +1,11 @@
+//
+//  User.swift
+//  ClapEducationiOS
+//
+//  Created by Grace Nho on 2024/03/21.
+//
+
+import Foundation
+
+struct User{
+}
--- ClapEducationiOS/ClapEducationiOS/Views/LoginView.swift
+++ ClapEducationiOS/ClapEducationiOS/Views/LoginView.swift
@@ -6,18 +6,49 @@
 //
 
 import SwiftUI
-
 struct LoginView: View {
+    @State private var username: String = ""
+    @State private var password:
+    String = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image("Logo")
+                .resizable()
+                .frame(width:85.0, height: 90.0).padding([.bottom], 20)
+            VStack(alignment: .leading){
+                Text("Email")
+                    .fontWeight(.bold)
+                TextField(
+                    "Email Address",
+                    text: $username
+                )
+                .padding(12)
+                .border(.secondary)
+                .cornerRadius(5)
+                .frame(width:320)
+            }.padding([.bottom], 20)
+            
+            
+            VStack(alignment: .leading){
+                Text("Password")
+                    .fontWeight(.bold)
+                SecureField(
+                    "Password",
+                    text: $username
+                )
+                .padding(12)
+                .border(.secondary)
+                .cornerRadius(5)
+                .frame(width:320)
+                .shadow(radius: /*@START_MENU_TOKEN@*/12/*@END_MENU_TOKEN@*/)
+            }
+            
+           
         }
-        .padding()
     }
+        
 }
+
 
 #Preview {
     LoginView()
